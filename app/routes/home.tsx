7395b8d18df83d96652915136211635522801d4a
Swastik Mishra <128520442+swastik-mishra25@users.noreply.github.com>
2025-09-01 10:54:20 +0530

    Refactor imports and update pdfjs-dist version to 5.3.93

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -5,6 +5,7 @@ import { usePuterStore } from "~/lib/puter";
 import { Link, useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 
+
 export function meta({ }: Route.MetaArgs) {
   return [
     { title: "Talentify.AI" },
